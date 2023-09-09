3cd969dde64ae5ad620f8d578659c3ed06f3cf71
yoons02 <kys5037k@naver.com>
2023-09-09 16:40:14 +0900

    modify main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 name = input("Enter your Name: ")
-std_num = input("Enter you Student Number: ")
+std_num = input("Enter your Student Number: ")
+major = input("Enter your major: ")
 
 print("\nHello World!!")
 print(f"Name: {name}")
 print(f"Student Number: {std_num}")
+print(f"Major: {major}")
